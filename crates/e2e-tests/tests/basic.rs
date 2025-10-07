6943ca9677ca63cb54cac6ae63f0f62e7aae2104
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2025-10-07 17:03:16 +0200

    feat(e2e): add e2e test structure using testcontainers

---
+++ crates/e2e-tests/tests/basic.rs
@@ -0,0 +1,38 @@
+use alloy::providers::{Provider, ProviderBuilder};
+use std::time::Duration;
+use tempo_e2e_tests::utils::setup_validators;
+use tokio::time::sleep;
+
+#[tokio::test]
+async fn basic_spin_up() {
+    let validators = setup_validators(4).await;
+
+    let first_node_url = validators
+        .get(0)
+        .unwrap()
+        .get_eth_rpc_url()
+        .await
+        .expect("there's more than one node");
+    let provider = ProviderBuilder::new()
+        .connect(&first_node_url)
+        .await
+        .unwrap();
+
+    for _ in 1..30 {
+        sleep(Duration::from_secs(1)).await;
+
+        let block_number_result = provider.get_block_number().await;
+
+        match block_number_result {
+            Ok(block_number) => {
+                println!("{}", block_number);
+                if block_number > 4 {
+                    break;
+                }
+            }
+            Err(_) => continue,
+        }
+    }
+
+    assert!(provider.get_block_number().await.is_ok_and(|i| i > 4));
+}
